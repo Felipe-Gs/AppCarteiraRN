fea4e8a250563449c6939238c2789e1fee6070a6
Felipe-Gs <felipegs2905@gmail.com>
2023-04-20 13:15:07 -0300

    feat: adicionado axios para fazer integração com o backend

---
+++ mobile/src/services/api.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+   baseURL: "http://192.168.1.105:3000",
+   headers: {
+      "Content-Type": "application/json",
+   },
+});
+
+export default api;
