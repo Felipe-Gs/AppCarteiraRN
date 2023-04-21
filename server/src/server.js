1a6c12c08070f400f329030db5b4bba7a46d6544
Felipe-Gs <felipegs2905@gmail.com>
2023-04-21 11:11:31 -0300

    feat: adicionado as rotasuario no server

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
+const rotasUsuario = require("./RotasUsuario");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(rotasUsuario);
 app.use("/", routes);
 
 app.listen(3000, () => {
