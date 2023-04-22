ff40a0154d67a04d7be6a7605210f4612f021c9b
Felipe-Gs <felipegs2905@gmail.com>
2023-04-22 14:07:39 -0300

    feat: adicionado dados da tela de notificação

---
+++ mobile/src/utils/NotificacaoText.ts
@@ -0,0 +1,44 @@
+export const NotificacaoText = [
+   {
+      id: "1",
+      data: "29 Abril 2023",
+      title: "Voce recebeu 100 R$ de Felipe Silva",
+      desc: "Pago no debito",
+   },
+   {
+      id: "2",
+      data: "29 Abril 2023",
+      title: "Voce recebeu 400 R$ de Felipe Silva",
+      desc: "Pago no credito",
+   },
+   {
+      id: "3",
+      data: "29 Abril 2023",
+      title: "Voce recebeu 20 R$ de Felipe Silva",
+      desc: "Pago no pix",
+   },
+   {
+      id: "4",
+      data: "29 Abril 2023",
+      title: "Voce recebeu 50 R$ de Felipe Silva",
+      desc: "Pago no debito",
+   },
+   {
+      id: "5",
+      data: "29 Abril 2023",
+      title: "Voce recebeu 200 R$ de Felipe Silva",
+      desc: "Pago no debito",
+   },
+   {
+      id: "6",
+      data: "29 Abril 2023",
+      title: "Voce recebeu 100 R$ de Felipe Silva",
+      desc: "Pago no credito",
+   },
+   {
+      id: "7",
+      data: "29 Abril 2023",
+      title: "Voce recebeu 200 R$ de Felipe Silva",
+      desc: "Pago no pix",
+   },
+];
