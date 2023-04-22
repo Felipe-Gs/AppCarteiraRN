feaacf3c1d49897c08b90b7fdc4e0f84ea097b39
Felipe-Gs <felipegs2905@gmail.com>
2023-04-22 14:07:23 -0300

    feat: adicionado dados fake da tela de dados

---
+++ mobile/src/utils/ConfigOpcoes.ts
@@ -0,0 +1,14 @@
+export const DadosConfig = [
+   {
+      titulo: "Connect account",
+   },
+   {
+      titulo: "Privacy and security",
+   },
+   {
+      titulo: "Login settings",
+   },
+   {
+      titulo: "Service senter",
+   },
+];
