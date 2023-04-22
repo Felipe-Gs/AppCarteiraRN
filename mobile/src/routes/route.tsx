213abe93f81b05689097d77ce4812b5674075752
Felipe-Gs <felipegs2905@gmail.com>
2023-04-22 14:05:18 -0300

    feat: adicionado rota Cartão

--- mobile/src/routes/route.tsx
+++ mobile/src/routes/route.tsx
@@ -7,6 +7,7 @@ import Home from "../screens/Home/Home";
 import Login from "../screens/Login/Login";
 import Cadastro from "../screens/Cadastro/Cadastro";
 import RecuperarSenha from "../screens/RecuperarSenha/RecuperarSenha";
+import Cartao from "../screens/Cartao/Cartao";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +18,7 @@ export function Route() {
          <Stack.Screen name="Login" component={Login} />
          <Stack.Screen name="Cadastro" component={Cadastro} />
          <Stack.Screen name="RecuperarSenha" component={RecuperarSenha} />
+         <Stack.Screen name="Cartao" component={Cartao} />
          {/* <Stack.Screen name="homeTabs" component={TabNavigation} /> */}
       </Stack.Navigator>
    );
