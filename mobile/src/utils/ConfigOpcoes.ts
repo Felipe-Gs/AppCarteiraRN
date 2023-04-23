e3aea6be99898cff75dffa33cc833b18957a5108
Felipe-Gs <felipegs2905@gmail.com>
2023-04-23 13:42:17 -0300

    feat: configuração das opces mudadas

--- mobile/src/utils/ConfigOpcoes.ts
+++ mobile/src/utils/ConfigOpcoes.ts
@@ -1,14 +1,39 @@
-export const DadosConfig = [
+import { useNavigation } from "@react-navigation/native";
+interface IdadosConfig {
+   titulo: string;
+   nameIcon?: string;
+   navegar?: () => void;
+}
+
+const { navigate } = useNavigation();
+
+export const DadosConfig: IdadosConfig[] = [
    {
-      titulo: "Connect account",
+      titulo: "Profile",
+      nameIcon: "account",
+      navegar() {
+         navigate("Home");
+      },
    },
    {
-      titulo: "Privacy and security",
+      titulo: "Notifications",
+      nameIcon: "bell",
+      navegar() {
+         navigate("Home");
+      },
    },
    {
       titulo: "Login settings",
+      nameIcon: "key",
+      navegar() {
+         navigate("Home");
+      },
    },
    {
       titulo: "Service senter",
+      nameIcon: "account-group",
+      navegar() {
+         navigate("Home");
+      },
    },
 ];
