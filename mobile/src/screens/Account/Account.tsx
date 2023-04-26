39c927d88fdabd609c82aa07301a33c2ada26e89
Felipe-Gs <felipegs2905@gmail.com>
2023-04-26 16:21:21 -0300

    feat: adicionado dados direto da API

--- mobile/src/screens/Account/Account.tsx
+++ mobile/src/screens/Account/Account.tsx
@@ -6,8 +6,11 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { Avatar } from "react-native-paper";
 
 import ClickNavagar from "../../components/ClickNavagar";
-
+import useAuth from "../../hook/useAuth";
+// https://reactnative.dev/docs/typescript
 const Account = () => {
+   const { dados } = useAuth();
+
    const { goBack, navigate } = useNavigation();
    return (
       <View style={styles.container}>
@@ -24,14 +27,11 @@ const Account = () => {
                size={110}
                source={require("../../assets/foto.jpeg")}
             />
-            <Text style={{ fontSize: 20 }}>Felipe Gomes</Text>
+            <Text style={{ fontSize: 20 }}>{dados.nome}</Text>
          </View>
 
          <View style={styles.viewText}>
-            <ClickNavagar
-               titulo="contatofelipegomes.dev@gmail.com"
-               nameIcon="email"
-            />
+            <ClickNavagar titulo={dados.email} nameIcon="email" />
             <ClickNavagar titulo="1300" nameIcon="cash" />
             <ClickNavagar titulo="Alto da Irmandade" nameIcon="city" />
             <ClickNavagar titulo="62910000" nameIcon="map-marker-radius" />
